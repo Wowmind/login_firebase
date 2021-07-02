22c8f713bf47d1c427c4ead5a777050dc28d5b84
Taiwo <Bellotaiwoa30@gmail.com>
2021-07-02 12:03:29 +0100

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,72 @@ import 'package:login_fire/signup.dart';
 
 import 'login.dart';
 
-void main() async{
-
+void main(){
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _int = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LogInPage(),
-      routes: <String, WidgetBuilder> {
-        '/landingpage' : (BuildContext context)=> new MyApp(),
-        '/signup' : (BuildContext context)=> new SignUpPage(),
-        '/homepage' : (BuildContext context)=> HomePage(),
-      },
+    return FutureBuilder(
+      future: _int,
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+        }else if(snapshot.hasError){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Center(
+                child: Column(
+                  children: [
+                    Icon(Icons.error),
+                    Text('Something went wrong !')
+                  ],
+                ),
+              ),
+            )
+          );
+        } else {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: LogInPage(),
+              routes: <String, WidgetBuilder>{
+                '/landingpage': (BuildContext context) => new MyApp(),
+                '/signup': (BuildContext context) => new SignUpPage(),
+                '/homepage': (BuildContext context) => HomePage(),
+              },
+            );
+          }
+
+        }
+        );
+  }
+}
+
+class ErrorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Icon(Icons.error),
+            Text('Something went wrong !')
+          ],
+        ),
+      ),
     );
   }
 }
